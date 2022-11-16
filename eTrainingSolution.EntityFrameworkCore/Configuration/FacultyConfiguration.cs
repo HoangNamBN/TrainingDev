f57bc91a39d03d4aa87c2c03ad80d60a092b2e18
NamNV8 <NamNV8@NAMNV8-D8>
2022-11-16 16:15:51 +0700

    configure using fluent api

---
+++ eTrainingSolution.EntityFrameworkCore/Configuration/FacultyConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using eTrainingSolution.EntityFrameworkCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eTrainingSolution.EntityFrameworkCore.Configuration
+{
+    public class FacultyConfiguration : IEntityTypeConfiguration<Faculty>
+    {
+        public void Configure(EntityTypeBuilder<Faculty> builder)
+        {
+            // khi chuyển sang sql server thì tên bản sẽ là Classes
+            builder.ToTable("Faculty");
+            // cấu hình khóa chính sẽ là thuộc tính ID
+            builder.HasKey(x => x.ID);
+            // bắt buộc trường ClassName phải nhập thông tin
+            builder.Property(x => x.FacultyName).IsRequired().IsUnicode();
+            // cấu hình khóa ngoại
+            builder.HasOne(x => x.Schools).WithMany(g => g.Faculties);
+        }
+    }
+}
