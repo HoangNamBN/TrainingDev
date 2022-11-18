4a03e0683bb7812d8d2166c178a5336d82a883d3
namnv <namnv@DESKTOP-MKRJMS0>
2022-11-19 00:41:59 +0700

    create SchoolController điều hướng xử lý logic - CRUD

---
+++ eTrainingSolution.WebApp/Areas/Identity/Controllers/SchoolController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    [Route("/School/[action]")]
+    public class SchoolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
