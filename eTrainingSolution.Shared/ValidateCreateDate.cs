de052ddc1bbc302e29346a1473a71e7dc3bd3758
namnv <namnv@DESKTOP-MKRJMS0>
2022-11-19 00:38:51 +0700

    validate cho ngày thành lập

---
+++ eTrainingSolution.Shared/ValidateCreateDate.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTrainingSolution.Shared
+{
+    public class ValidateCreateDate : ValidationAttribute
+    {
+        /// <summary>
+        /// validate trường ngày thành lập (ngày thành lập phải lớn hơn ngày tạo)
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override bool IsValid(object? value)
+        {
+            if (value == null) return false;
+            DateTime dateTime = Convert.ToDateTime(value);
+            return dateTime <= DateTime.Now;
+        }
+    }
+}
