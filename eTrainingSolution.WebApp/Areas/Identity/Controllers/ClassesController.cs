fa6e20b7de5d7a21075b678f8b9f8a10aad976c0
NamNV8 <NamNV8@NAMNV8-D8>
2022-11-21 18:25:59 +0700

    xử lý logic cho lớp và khoa

---
+++ eTrainingSolution.WebApp/Areas/Identity/Controllers/ClassesController.cs
@@ -0,0 +1,144 @@
+﻿using eTrainingSolution.EntityFrameworkCore;
+using eTrainingSolution.EntityFrameworkCore.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    [Route("/Classes/[action]")]
+    public class ClassesController : Controller
+    {
+        private readonly eTrainingDbContext _eTrainingDbContext;
+
+        public ClassesController(eTrainingDbContext eTrainingDbContext) { 
+            _eTrainingDbContext = eTrainingDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var classesDbContext = _eTrainingDbContext.Classrooms.Include(f => f.Facultys);
+            return View(await classesDbContext.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ID, ClassName, ClassCapacity, CreateDate, CreatedBy, FacultyID")] Classroom classes)
+        {
+            if(ModelState.IsValid)
+            {
+                classes.ID= Guid.NewGuid();
+                _eTrainingDbContext.Add(classes);
+                await _eTrainingDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName", classes.FacultyID);
+            return View(classes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid? id)
+        {
+            // Kiểm tra nếu id null hoặc không tồn tại khoa thì return NotFound()
+            if(id == null || _eTrainingDbContext.Classrooms == null)
+            {
+                return NotFound();
+            }
+            // lấy thông tin của class kèm theo thông tin Facultys
+            var classDb = await _eTrainingDbContext.Classrooms.Include(g=> g.Facultys).FirstOrDefaultAsync(m => m.ID == id);
+            if(classDb == null)
+            {
+                return NotFound();
+            }
+            return View(classDb);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if(id == null || _eTrainingDbContext.Classrooms == null)
+            {
+                return NotFound();
+            }
+            var classDbContext = await _eTrainingDbContext.Classrooms.FindAsync(id);
+            if(classDbContext == null)
+            {
+                return NotFound();
+            }
+            ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName", classDbContext.FacultyID);
+            return View(classDbContext);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid? id, 
+            [Bind("ID, ClassName, ClassCapacity, CreateDate, CreatedBy, FacultyID")] Classroom classes)
+        {
+            if (id != classes.ID)
+            {
+                // trả về kết quả không tìm thấy
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var classDb = _eTrainingDbContext.Classrooms.Find(id);
+                    _eTrainingDbContext.Entry(classDb).CurrentValues.SetValues(classes);
+                    await _eTrainingDbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName", classes.FacultyID);
+            return View(classes);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if(id == null || _eTrainingDbContext.Classrooms == null)
+            {
+                return NotFound();
+            }
+            var classDbContext = await _eTrainingDbContext.Classrooms.Include(m => m.Facultys).FirstOrDefaultAsync(g => g.ID == id);
+            if(classDbContext == null)
+            {
+                return NotFound();
+            }
+            return View(classDbContext);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            if (_eTrainingDbContext.Classrooms == null)
+            {
+                return Problem("Entity set null!");
+            }
+            var classDbContext = await _eTrainingDbContext.Classrooms.FindAsync(id);
+            if(classDbContext == null)
+            {
+                return NotFound();
+            }
+            // Xóa theo ID
+            _eTrainingDbContext.Classrooms.Remove(classDbContext);
+            await _eTrainingDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ eTrainingSolution.WebApp/Areas/Identity/Controllers/FacultyController.cs
@@ -0,0 +1,160 @@
+﻿using eTrainingSolution.EntityFrameworkCore;
+using eTrainingSolution.EntityFrameworkCore.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+
+namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    [Route("/Faculty/[action]")]
+    public class FacultyController : Controller
+    {
+        private readonly eTrainingDbContext _eTrainingDbContext;
+
+        public FacultyController(eTrainingDbContext eTrainingDbContext)
+        {
+            _eTrainingDbContext = eTrainingDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            if(_eTrainingDbContext.Schools == null)
+            {
+                return Problem("null");
+            }
+            // thực hiện query để trả về list danh sách các phần tử
+            var eTrainDbContext = _eTrainingDbContext.Facultys.Include(f => f.Schools);
+            return View(await eTrainDbContext.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName");
+            return View();
+        }
+
+        /// <summary>
+        /// Bind("ID, FacultyName, CapacityOfFaculty, CreateDate, CreatedBy, SchoolID"): tạo ra 1 list item add vào BindAtrribute
+        /// </summary>
+        /// <param name="faculty"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ID, FacultyName, CapacityOfFaculty, CreateDate, CreatedBy, SchoolID")] Faculty faculty)
+        {
+            if(ModelState.IsValid)
+            {
+                faculty.ID = Guid.NewGuid();
+                _eTrainingDbContext.Add(faculty);
+                await _eTrainingDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName", faculty.SchoolID);
+            return View(faculty);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid? id)
+        {
+            if(id == null || _eTrainingDbContext.Facultys == null)
+            {
+                return NotFound();
+            }
+            // câu lệnh query để truy vấn giữa ID của bảng Facultys với id của bảng Schools
+            var faculitiesDbContext = await _eTrainingDbContext.Facultys.Include(g => g.Schools).FirstOrDefaultAsync(m => m.ID == id);
+            if(faculitiesDbContext == null)
+            {
+                return NotFound();
+            }
+            return View(faculitiesDbContext);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _eTrainingDbContext.Facultys == null)
+            {
+                return NotFound();
+            }
+            // tìm Khoa theo ID
+            var faculitiesDbContext = await _eTrainingDbContext.Facultys.FindAsync(id);
+            if(faculitiesDbContext == null)
+            {
+                return NotFound();
+            }
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName", faculitiesDbContext.SchoolID);
+            return View(faculitiesDbContext);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid? id, [Bind("ID, FacultyName, CapacityOfFaculty, CreateDate, CreatedBy, SchoolID")] Faculty faculty)
+        {
+            if (id == null || id != faculty.ID)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var facultyesDb = _eTrainingDbContext.Facultys.Find(id);
+                    _eTrainingDbContext.Entry(facultyesDb).CurrentValues.SetValues(faculty);
+                    await _eTrainingDbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName", faculty.SchoolID);
+            return View(faculty);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var faculitiesDbContext = await _eTrainingDbContext.Facultys.Include(m => m.Schools).FirstOrDefaultAsync(_ => _.ID == id);
+            if(faculitiesDbContext == null)
+            {
+                return NotFound();
+            }
+            return View(faculitiesDbContext);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(Guid? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            if(_eTrainingDbContext.Facultys == null)
+            {
+                return Problem("Entity set null");
+            }
+            // Lấy thông tin Khoa theo ID
+            var faculytiesDbContext = await _eTrainingDbContext.Facultys.FindAsync(id);
+            if(faculytiesDbContext!= null)
+            {
+                // Tìm ra danh sách các lớp trong Khoa được chọn
+                var classDbContext = await _eTrainingDbContext.Classrooms.Where(m=> m.FacultyID == id).ToListAsync();
+                if(classDbContext!= null)
+                {
+                    _eTrainingDbContext.Classrooms.RemoveRange(classDbContext);
+                }
+                _eTrainingDbContext.Facultys.Remove(faculytiesDbContext);
+            }
+            await _eTrainingDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
