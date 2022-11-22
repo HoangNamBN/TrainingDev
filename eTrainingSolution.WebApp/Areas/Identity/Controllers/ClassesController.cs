09a9abd16f543957bb77c9609a919dea713ba8c5
NamNV8 <NamNV8@NAMNV8-D8>
2022-11-22 18:53:28 +0700

    edit ClassesController

--- eTrainingSolution.WebApp/Areas/Identity/Controllers/ClassesController.cs
+++ eTrainingSolution.WebApp/Areas/Identity/Controllers/ClassesController.cs
@@ -19,28 +19,49 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var classesDbContext = _eTrainingDbContext.Classrooms.Include(f => f.Facultys);
+            // lấy thông tin của class kèm theo thông tin của Facultys
+            var classesDbContext = _eTrainingDbContext.Classrooms.Include(f => f.Facultys).Include(f => f.Schools);
             return View(await classesDbContext.ToListAsync());
         }
 
+        /// <summary>
+        /// Nghiệp vụ: 
+        ///     - Chuyển hướng sang form Detail là Tạo mới
+        ///     - Nhập các thông tin cần thiết trong đó có lựa chọn Khoa cho lớp học. Thông tin này sẽ được truyền từ Controller sang View để hứng kết quả
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> Create()
         {
+            // lấy dữ liệu từ Controller chuyển sang cho View
+            // ViewData có kiểu là Dictionary nên sẽ chứa cặp key-value
+            // ViewData có giá trị sẽ bị xóa khi mà chuyển hướng
             ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName");
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName");
             return View();
         }
 
+        /// <summary>
+        /// Nghiệp vụ:
+        ///     - khi nhấn vào button đăng ký => Controller nhận được request
+        ///     - Các thông tin được nhập sẽ được truyền đến
+        /// </summary>
+        /// <param name="classes"></param>
+        /// <returns></returns>
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("ID, ClassName, ClassCapacity, CreateDate, CreatedBy, FacultyID")] Classroom classes)
+        public async Task<IActionResult> Create([Bind("ID, ClassName, ClassCapacity, CreateDate, CreatedBy, FacultyID, SchoolID")] Classroom classes)
         {
+            // check kiểm tra xem giá trị của các trường thông tin có là hợp lệ hay không
             if(ModelState.IsValid)
             {
+                // tạo ID cho classes
                 classes.ID= Guid.NewGuid();
                 _eTrainingDbContext.Add(classes);
                 await _eTrainingDbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName", classes.FacultyID);
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName", classes.SchoolID);
             return View(classes);
         }
 
@@ -53,7 +74,7 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
                 return NotFound();
             }
             // lấy thông tin của class kèm theo thông tin Facultys
-            var classDb = await _eTrainingDbContext.Classrooms.Include(g=> g.Facultys).FirstOrDefaultAsync(m => m.ID == id);
+            var classDb = await _eTrainingDbContext.Classrooms.Include(g=> g.Facultys).Include(g => g.Schools).FirstOrDefaultAsync(m => m.ID == id);
             if(classDb == null)
             {
                 return NotFound();
@@ -74,14 +95,22 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
                 return NotFound();
             }
             ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName", classDbContext.FacultyID);
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName", classDbContext.SchoolID);
             return View(classDbContext);
         }
 
+        /// <summary>
+        /// Bind: chỉ định các thuộc tính nào của lớp Model Classroom muốn liên kết
+        /// Nếu muốn bỏ bớt các thuộc tính thì thêm Exclude
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="classes"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> Edit(Guid? id, 
-            [Bind("ID, ClassName, ClassCapacity, CreateDate, CreatedBy, FacultyID")] Classroom classes)
+            [Bind("ID, ClassName, ClassCapacity, CreateDate, CreatedBy, FacultyID, SchoolID")] Classroom classes)
         {
-            if (id != classes.ID)
+            if (id != classes.ID || id == null)
             {
                 // trả về kết quả không tìm thấy
                 return NotFound();
@@ -91,6 +120,7 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
                 try
                 {
                     var classDb = _eTrainingDbContext.Classrooms.Find(id);
+                    // sử dụng Entry của DbContext, SetValues để cập nhật các trường thông tin 
                     _eTrainingDbContext.Entry(classDb).CurrentValues.SetValues(classes);
                     await _eTrainingDbContext.SaveChangesAsync();
                 }
@@ -101,6 +131,7 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewData["FacultyID"] = new SelectList(_eTrainingDbContext.Facultys, "ID", "FacultyName", classes.FacultyID);
+            ViewData["SchoolID"] = new SelectList(_eTrainingDbContext.Schools, "Id", "SchoolName", classes.SchoolID);
             return View(classes);
         }
 
@@ -111,7 +142,8 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
             {
                 return NotFound();
             }
-            var classDbContext = await _eTrainingDbContext.Classrooms.Include(m => m.Facultys).FirstOrDefaultAsync(g => g.ID == id);
+            // lấy thông tin của class thông qua ID và kèm theo thông tin của Facultys
+            var classDbContext = await _eTrainingDbContext.Classrooms.Include(m => m.Facultys).Include(m => m.Schools).FirstOrDefaultAsync(g => g.ID == id);
             if(classDbContext == null)
             {
                 return NotFound();
@@ -119,6 +151,11 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
             return View(classDbContext);
         }
 
+        /// <summary>
+        /// Do cùng truyền vào tham số là id nên đối với Delete của HttpPost thì đổi tên thành DeleteConfirmed và thêm ActionName
+        /// </summary>
+        /// <param name="id">id của lớp muốn xóa</param>
+        /// <returns></returns>
         [HttpPost, ActionName("Delete")]
         public async Task<IActionResult> DeleteConfirmed(Guid? id)
         {
@@ -130,6 +167,7 @@ namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
             {
                 return Problem("Entity set null!");
             }
+            // lấy thông tin class theo ID tìm được
             var classDbContext = await _eTrainingDbContext.Classrooms.FindAsync(id);
             if(classDbContext == null)
             {
