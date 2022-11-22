14f089b1daf456bde452a5e5664f12129c80144f
NamNV8 <NamNV8@NAMNV8-D8>
2022-11-22 18:52:08 +0700

    validate tên lớp và tên Khoa

---
+++ eTrainingSolution.EntityFrameworkCore/Validation/ValidateClassNameIsUnicode.cs
@@ -0,0 +1,49 @@
+﻿using eTrainingSolution.EntityFrameworkCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTrainingSolution.EntityFrameworkCore.Validation
+{
+    public class ValidateClassNameIsUnicode : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(value == null)
+            {
+                return new ValidationResult("Bạn cần nhập thông tin tên lớp");
+            }
+
+            // tạo ra obejct Instance
+            var classInstance = (Classroom)validationContext.ObjectInstance;
+
+            var _context = (eTrainingDbContext)validationContext?.GetService(typeof(eTrainingDbContext));
+
+            // lấy thông tin các trường học đang đăng ký
+            var schooolDbContext = _context.Schools?.ToList();
+
+            foreach (var itemSchool in schooolDbContext)
+            {
+                if(itemSchool.Id == classInstance.SchoolID)
+                {
+                    var facultysDbContext = _context.Facultys.Where(m => m.SchoolID == itemSchool.Id).ToList();
+                    foreach (var itemFaculty in facultysDbContext)
+                    {
+                        var classDbContext = _context.Classrooms?.Where(m => m.FacultyID == itemFaculty.ID).ToList();
+                        foreach (var classDb in classDbContext)
+                        {
+                            if (value.ToString().ToUpper() == classDb.ClassName.ToUpper())
+                            {
+                                return new ValidationResult("Trường " + itemSchool.SchoolName + " đã tồn tại lớp này tại Khoa " + itemFaculty.FacultyName);
+                            }
+                        }
+                    }
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
