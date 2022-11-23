77f8b8ef272b77fafff6263c497c021042a6ae05
namnv <namnv@DESKTOP-MKRJMS0>
2022-11-23 23:12:21 +0700

    create new ManageController

---
+++ eTrainingSolution.WebApp/Areas/Identity/Controllers/ManageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eTrainingSolution.WebApp.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    [Route("/Account/Manage/[action]")]
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
