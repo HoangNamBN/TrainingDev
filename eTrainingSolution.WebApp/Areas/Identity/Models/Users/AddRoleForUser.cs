3c73710de5b7839bf978db0dc3d4e00fecd3d3c0
NamNV8 <NamNV8@NAMNV8-D8>
2022-11-23 19:00:50 +0700

    tạo model cho user

---
+++ eTrainingSolution.WebApp/Areas/Identity/Models/Users/AddRoleForUser.cs
@@ -0,0 +1,16 @@
+﻿using eTrainingSolution.EntityFrameworkCore.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTrainingSolution.WebApp.Areas.Identity.Models.Users
+{
+    public class AddRoleForUser
+    {
+        public User? user { get; set; }
+
+        /// <summary>
+        /// khai báo một mảng string chứa các quyền mà người dùng có thể có
+        /// </summary>
+        [Display(Name ="Các role được gán cho user")]
+        public string[]? RoleNames { get; set; }
+    }
+}
---
+++ eTrainingSolution.WebApp/Areas/Identity/Models/Users/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using eTrainingSolution.EntityFrameworkCore.Entities;
+
+namespace eTrainingSolution.WebApp.Areas.Identity.Models.Users
+{
+    public class UserModel
+    {
+        /// <summary>
+        /// tổng số lượng người dùng
+        /// </summary>
+        public int totalUser { get; set; }
+
+        /// <summary>
+        /// tổng số trang
+        /// </summary>
+        public int countPages { get; set; }
+
+        /// <summary>
+        /// thiết lập số phần tử hiển thị tại mỗi trang
+        /// </summary>
+        public int ITEM_PER_PAGE { get; set; } = 5;
+
+        /// <summary>
+        /// số trang hiện tại
+        /// </summary>
+        public int currentPage { get; set; }
+
+        public List<UserAndRole>? users { get; set; }
+    }
+
+    public class UserAndRole : User
+    {
+        public string? RoleName { get; set; }
+    }
+}
