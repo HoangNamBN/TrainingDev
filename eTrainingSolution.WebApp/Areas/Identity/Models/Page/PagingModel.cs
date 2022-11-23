eef00047a6fa5edadddd8cbb9c1d237d920b6f9c
NamNV8 <NamNV8@NAMNV8-D8>
2022-11-23 18:58:47 +0700

    phân trang

---
+++ eTrainingSolution.WebApp/Areas/Identity/Models/Page/PagingModel.cs
@@ -0,0 +1,20 @@
+﻿namespace eTrainingSolution.WebApp.Areas.Identity.Models.Page
+{
+    public class PagingModel
+    {
+        /// <summary>
+        /// Trang hiện tại
+        /// </summary>
+        public int currentPage { get; set; }
+
+        /// <summary>
+        /// Tổng số trang
+        /// </summary>
+        public int CountPage { get; set; }
+
+        /// <summary>
+        /// Trả về Url tương ứng với trang
+        /// </summary>
+        public Func<int?, string> GenerateUrl { get; set; } = default!;
+    }
+}
